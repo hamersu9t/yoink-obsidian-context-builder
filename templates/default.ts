ba2c7a112f066dfbe259a7297c5e16e5e0520f53
richard_kim <4302485+hamersu9t@users.noreply.github.com>
2024-07-14 18:15:58 -0400

    update default

--- templates/default.ts
+++ templates/default.ts
@@ -1,8 +1,13 @@
-export default `This context is pulled from my Obsidian notes. It represents my network of linked notes based on the Obsidian graph. The context is sorted by most relevant to least relevant based on the proximity to the main note, the number of times it was referenced via a link, and the date it was last updated. Links within a note link to other notes are indicated with the [[note title]] syntax. Here is the primary note, please treat it as the most relevant for the conversation that will follow:
+export default `This context is pulled from my Obsidian notes. It represents my network of linked notes based on the Obsidian graph. The context is sorted by most relevant to least relevant based on:
+- proximity: the number of links it takes to get to the primary note
+- reference count: the number of times the note is linked by other relevant notes
+- recency: the date the note was last updated
+
+Links within a note link to other notes are indicated with the [[note title]] syntax. Here is the primary note, please treat it as the most relevant for the conversation that will follow:
 
 CONTEXT START
 PRIMARY NOTE HAS PATH: {{{primaryNote.path}}}
-METADATA: this is the main note, number of times referenced: {{{primaryNote.count}}}, date updated: {{{primaryNote.dateUpdated}}}
+METADATA: this is the main note, date updated: {{{primaryNote.dateUpdated}}}
 
 PRIMARY NOTE CONTENT:
 {{{primaryNote.content}}}
